215bd895036530c2f1f9ed372221f1eb5986a479
RahulAtDure <rahul@duretechnologies.com>
2018-02-07 10:52:06 +0530

    Added improved logic of calculating Prime Number
    	Improved the logic of testing whether the number is prime-number
    	or not. I used logic of using square-root.

--- problem07.js
+++ problem07.js
@@ -12,7 +12,8 @@
 			do{
 				
 				if(x !== 1 && x !== 2 && x !== 3){
-					isPrime = isPrimeNumber(x);
+					//isPrime = isPrimeNumber(x);
+					isPrime = isPrimeImproved(x);
 					if(isPrime === false){
 						x++;
 					}
@@ -56,6 +57,38 @@
 
 		return true;
 	} // end of isPrimeNumber
+
+
+	// is Prime number with improved logic.
+	// checks prime number on the basis of 
+	// square-root
+	function isPrimeImproved(num){
+		var limit = Math.sqrt(num);
+		limit = Math.floor(limit);
+
+		if(num <= 1){
+			return false;
+		}
+
+		// if num is 2 or 3, than its a prime number
+		if(num === 2 || num === 3){
+			return true;
+		}
+		
+		// check if number is divisible by 2, 
+		// if yes then its not a prime number.
+		if(num%2 === 0){
+			return false;
+		}
+		
+		for(var x = 2; x <= limit; x++){
+			if(num%x === 0){
+				return false;
+			}
+		}
+
+		return true;
+	};
 
 
 	function getCommandLineVariables(){
